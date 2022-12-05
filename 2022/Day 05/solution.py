3ed6cb2f1fd768f5b8bd1deef99f5b8c3c8f1ab9
damien-deathstalker <bekoekofi@gmail.com>
2022-12-05 05:55:39 +0000

    Day 5 Solved
    So I cheated a bit on this one.
    I wanted the points so I formatted the input a bit to have them stack horizontally so I could read it quickly.
    The next thing I might have to look at is how I'd read the data as it is. But for now, this works! :joy:

--- 2022/Day 05/solution.py
+++ 2022/Day 05/solution.py
@@ -1,4 +1,6 @@
-import os
+import os, re
+
+
 class Answers:
     def __init__(self, partOne: int = 0, partTwo: int = 0) -> None:
         self.__partOne = partOne
@@ -22,21 +24,52 @@ def readFile(filepath):
 
 
 def solutionOne(fileName: str):
-    result = 0
+    result = ""
+    crate_stack:dict[str, list[str]] = {}
+    crate_regex = r"([A-Z])"
+    instruction_regex = r"^move (?P<number_to_move>[0-9]+) from (?P<origin>[0-9]+) to (?P<destination>[0-9]+)$"
+    count = 1
     for line in readFile(f"{fileName}.txt"):
-        pass
+        crates = re.findall(crate_regex, line)
+        if len(crates) > 0:
+            crates.reverse()
+            crate_stack[str(count)] = crates
+            count += 1
+        instruction = re.search(instruction_regex, line)
+        if instruction:
+            instructions = instruction.groupdict()
+            for number in range(0, int(instructions["number_to_move"])):
+                crate_stack[instructions["destination"]].append(
+                    crate_stack[instructions["origin"]].pop()
+                )
+    result = "".join(x[-1] for x in crate_stack.values())
     return result
 
 
 def solutionTwo(fileName: str):
-    result = 0
+    result = ""
+    crate_stack:dict[str, list[str]] = {}
+    crate_regex = r"([A-Z])"
+    instruction_regex = r"^move (?P<number_to_move>[0-9]+) from (?P<origin>[0-9]+) to (?P<destination>[0-9]+)$"
+    count = 1
     for line in readFile(f"{fileName}.txt"):
-        pass
+        crates = re.findall(crate_regex, line)
+        if len(crates) > 0:
+            crates.reverse()
+            crate_stack[str(count)] = crates
+            count += 1
+        instruction = re.search(instruction_regex, line)
+        if instruction:
+            instructions = instruction.groupdict()
+            crate_stack[instructions["destination"]] += [crts for crts in crate_stack[instructions["origin"]][-int(instructions["number_to_move"]):]]
+            for number in range(0, int(instructions["number_to_move"])):
+                crate_stack[instructions["origin"]].pop()
+    result = "".join(x[-1] for x in crate_stack.values())
     return result
 
 
 if __name__ == "__main__":
-    answer = Answers()
+    answer = Answers("CMZ", "MCD")
     assert solutionOne("test") == answer.partOne
     print("Part One: ", solutionOne("input"))
     assert solutionTwo("test") == answer.partTwo
